35e575751d3c7201fd8bd4e5a690af7ad924c388
ibrahimFN <89642812+ibrahimFN@users.noreply.github.com>
2021-11-20 19:17:37 +0300

    Update and rename ressources/index.js to المصادر/index.js

---
+++ المصادر/index.js
@@ -0,0 +1,13 @@
+module.exports.Channels = Object.freeze({
+    //Copy paste your channel ID for automatic post
+    battleroyalenews: "911647565370511431",
+    creativenews: "911647565370511431",
+    savetheworldnews: "911647565370511431",
+    emergencynotice: "911647565370511431",
+    newCos : "911647565370511431",
+    shop: "911647565370511431"
+})
+
+module.exports.Param = Object.freeze({
+    prefix: "*",
+})
